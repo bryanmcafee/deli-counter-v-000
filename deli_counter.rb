fde0008b8dd18ed80d88650a67ee5f3e568caaa5
Bryan McAfee <brymcafee@gmail.com>
2018-05-22 17:14:04 +0000

    automatically backed up by learn

---
+++ deli_counter.rb
@@ -0,0 +1,14 @@
+def line_method(numinline)
+  #lma = line method array
+  lma = []
+  if numinline.lenght == 0
+    puts "The line is currently empty"
+  else
+    lma.each.with_index(o) do |name, index|
+      lma.push("#{index}. #{name}")
+    end
+    puts "The line is: #{lma.join(" ")}"
+  end
+end
+
+      
